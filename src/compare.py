5f58fab01bf4ad0884ad35a19fa34fc1b20fd9f5
Matt Sutton <mcsutton@outlook.com>
2017-11-28 19:02:05 -0700

    created a base image comparison function and implemented in piCamera

--- src/compare.py
+++ src/compare.py
@@ -1,57 +1,17 @@
 import cv2
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import numpy as np
-import time
 
-first_cap = True
+def getDiff(base_image, current_image):
+    base_image = cv2.GaussianBlur(base_image, (9,9), 0)
+    current_image = cv2.GaussianBlur(current_image, (9,9), 0)
 
-# setup camera
-camera = PiCamera()
-camera.vflip = True
-camera.hflip = True
-camera.resolution = (640, 480)
-camera.framerate = 30
-camera.iso = 400 
-rawCapture = PiRGBArray(camera)
+    base_image_gray = cv2.cvtColor(base_image, cv2.COLOR_BGR2GRAY)
+    current_image_gray = cv2.cvtColor(current_image, cv2.COLOR_BGR2GRAY)
 
-time.sleep(0.5)
-
-camera.shutter_speed = camera.exposure_speed
-camera.exposure_mode = 'off'
-g = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = g
-
-time.sleep(0.5)
-
-fgbg = cv2.createBackgroundSubtractorMOG2(history=1000)
-
-for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
-    if first_cap:    
-        space = frame.array
-        space = cv2.GaussianBlur(space, (9,9), 0)
-        space_gray = cv2.cvtColor(space, cv2.COLOR_BGR2GRAY)
-        first_cap = False
-
-    image = frame.array
-    image = cv2.GaussianBlur(image, (9, 9), 0)
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    diff = cv2.absdiff(space_gray, image_gray)
+    diff = cv2.absdiff(base_image_gray, current_image_gray)
 
     _, mask = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
-    
-    mask = fgbg.apply(mask)
 
-    cv2.imshow('mask', mask)
-    cv2.imshow('diff', diff)
-    cv2.imshow('image', image)
-    cv2.imshow('space', space)
+    return diff
 
-    rawCapture.truncate(0)
 
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
-
-cv2.destroyAllWindows()
--- src/piCamera.py
+++ src/piCamera.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 from zones import Zone
 import uploadData
+from compare import getDiff
 
 def nothing(x):
     pass
@@ -13,8 +14,9 @@ def nothing(x):
 imageNumber = 1
 saveImage = True
 zones = [Zone()]
+base_image_capture = True
 
-uploadData.createArea("TestArea", "it's in a room on the north side")
+uploadData.createDomain("TestArea", "it's in a room on the north side")
 uploadData.createZone(zones)
 
 # setup camera
@@ -23,10 +25,19 @@ camera.vflip = True
 camera.hflip = True
 camera.resolution = (640, 480)
 camera.framerate = 32
+camera.iso = 400
 rawCapture = PiRGBArray(camera)
 
+time.sleep(0.1)
+
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+g = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = g
+
 # setup background subtractor
-fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=80, detectShadows=True)
+fgbg = cv2.createBackgroundSubtractorMOG2(history=1000)
 
 # setup window
 cv2.namedWindow('image')
@@ -38,9 +49,15 @@ heatMap = np.zeros((480, 640), dtype=np.uint8)
 time.sleep(0.1)
 
 for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
-    image = frame.array
+    if base_image_capture:
+        base_image = frame.array
+        base_image_capture = False
 
-    mask = fgbg.apply(image, 1)
+    image = frame.array
+    
+    diff = getDiff(base_image, image)
+
+    mask = fgbg.apply(diff)
 
     heatMap = cv2.addWeighted(heatMap, 0.97, mask, 0.03, 0)
 
@@ -52,15 +69,17 @@ for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=
     while time.localtime().tm_sec % 5 == 0:
         if saveImage == True:
             uploadData.updateZoneInstance(zones, heatMap_color)
-            uploadData.updateHeatmapInstance('frame')
+            uploadData.updateHeatmapInstance(heatMap_color)
             saveImage = False
             print "dataUploaded"
 
     saveImage = True
 
+    cv2.imshow('diff', diff)
     cv2.imshow('image', image)
     cv2.imshow('mask', mask)
     cv2.imshow('heatmap', heatMap_color)
+
     rawCapture.truncate(0)
 
     if cv2.waitKey(1) & 0xff == ord('q'):
